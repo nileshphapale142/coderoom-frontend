ff6201541809f960eabb870533447cf1abd91cf2
Nilesh Phapale <nileshphapale142@gmail.com>
2024-07-30 19:31:37 +0530

    Production Environment var added

--- api/axios.ts
+++ api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const backendApi = axios.create({
-  baseURL: process.env?.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://coderoom-backend.onrender.com', 
+  baseURL: process.env?.NODE_ENVIRONMENT === 'development' ? 'http://localhost:5000' : 'https://coderoom-backend.onrender.com', 
   withCredentials: true
 });
