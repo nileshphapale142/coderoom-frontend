38ccaf38f5fac0831b0ac6ef7341904ec184908b
nileshphapale142 <nileshphapale142@gmail.com>
2024-11-16 15:08:54 +0530

    Copy to clipboard not available alert added

--- components/Utils/codeBlock.tsx
+++ components/Utils/codeBlock.tsx
@@ -14,35 +14,39 @@ export const CodeBlock = ({
   const [isCopied, setIsCopied] = useState(false);
 
   const copyToClipboard = async () => {
+    if (!navigator.clipboard || !navigator.clipboard.writeText) {
+      console.error('Clipboard API is not supported in this environment.');
+      alert('Copying to clipboard is not supported in your browser.');
+      return;
+    }
+
     try {
       await navigator.clipboard.writeText(value);
       setIsCopied(true);
       setTimeout(() => setIsCopied(false), 2000); // Reset after 2 seconds
     } catch (err) {
       console.error('Failed to copy text: ', err);
+      alert('Failed to copy text. Please try again.');
     }
   };
 
   return (
-    <div className='relative'>
+    <div className="relative">
       <button
         onClick={copyToClipboard}
-        className='absolute right-0 top-0 z-50 mr-3 rounded-3 px-2 py-1 transition-all hover:bg-stone-200'
+        className="absolute right-0 top-0 z-50 mr-3 rounded-3 px-2 py-1 transition-all hover:bg-stone-200"
       >
         {isCopied ? 'Copied!' : 'Copy'}
       </button>
-      <div className='relative'>
+      <div className="relative">
         <SyntaxHighlighter
           language={language}
           style={oneLight}
           wrapLongLines={true}
           customStyle={{
-            // padding: '1em',
-            // paddingTop: '2em', // Make room for the copy button
             borderRadius: '5px',
             fontSize: '12px',
             backgroundColor: 'white',
-            // border: '1px solid #e1e4e8',
           }}
         >
           {value}
